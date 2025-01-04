ed2b37cc370e529bc5c20dc2cd697dd3606fb82d
kangsuyeong <ekdh16@naver.com>
2025-01-04 17:43:17 +0900

    feat : 5강 완료

---
+++ src/components/common/header/Header.tsx
@@ -0,0 +1,23 @@
+import styles from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      {/* 로고박스 */}
+      <div className={styles.header__logoBox}>
+        <img
+          src="src/assets/images/image-logo.png"
+          alt=""
+          className={styles.header__logoBox__logo}
+        />
+        <span className={styles.header__logoBox__title}>PhotoSplash</span>
+      </div>
+      {/* 프로필 박스 */}
+      <div className={styles.header__profileBox}>
+        <button className={styles.header__profileBox__button}>사진제출</button>
+        <button className={styles.header__profileBox__button}>북마크</button>
+        <span className={styles.header__profileBox__userName}>강수영</span>
+      </div>
+    </div>
+  );
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,9 +1,11 @@
+import Header from "@/components/common/header/Header";
 import styles from "./styles/index.module.scss";
 
 function index() {
   return (
     <div className={styles.page}>
       {/* 공통 헤더 UI 부분 */}
+      <Header />
       {/* 공통 네이버게이션 UI 부분 */}
       <div className={styles.page__contents}>
         <div className={styles.page__contents__introBox}>
@@ -11,7 +13,7 @@ function index() {
             <span className={styles.wrapper__title}>PhotoSplash</span>
             <span className={styles.wrapper__desc}>
               인터넷의 시각 자료 출처입니다. <br />
-              모든 지역에 잇는 크리에이터들의 지원을 받습니다.
+              모든 지역에 있는 크리에이터들의 지원을 받습니다.
             </span>
             {/* 검색창 UI 부분 */}
           </div>
