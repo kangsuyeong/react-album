8c68a0701828c463054793e1bfb1c07330a07721
kangsuyeong <ekdh16@naver.com>
2025-01-08 19:51:20 +0900

    feat : 12강 완료

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Header from "@/components/common/header/Header";
 import SearchBar from "@/components/common/searchBar/SearchBar";
 import Nav from "@/components/common/navigation/Nav";
@@ -9,40 +8,16 @@ import Card from "./components/Card";
 import styles from "./styles/index.module.scss";
 
 import { CardDTO } from "./types/card";
+import { useRecoilValue } from "recoil";
+import { imageData } from "@/recoil/selectors/imageSelectors";
 
 function index() {
-  const [imgUrls, setImgUrls] = useState([]);
-  const getData = async () => {
-    // 오픈 API 호출
-    const API_URL = "https://api.unsplash.com/search/photos";
-    const API_key = "BQxx4yY-cWYtU5z5C3dZB8GuBU_GbLKDjupgrCJPcG4";
-    const PER_PAGE = 30;
+  const imgSelector = useRecoilValue(imageData);
+  const [imgData, setImgData] = useState<CardDTO[]>([]);
 
-    const searchValue = "Korea";
-    const pageValue = 100;
-
-    try {
-      const res = await axios.get(
-        `${API_URL}?query=${searchValue}&client_id=${API_key}&page=${pageValue}&per_page=${PER_PAGE}`
-      );
-      console.log(res);
-      // res.data.results라는 배열을 활용할 예정
-
-      if (res.status === 200) {
-        setImgUrls(res.data.results);
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  const cardList = imgUrls.map((card: CardDTO) => {
+  const CARD_LIST = imgSelector.data.results.map((card: CardDTO) => {
     return <Card data={card} key={card.id} />;
   });
-
-  useEffect(() => {
-    getData();
-  }, []);
 
   return (
     <div className={styles.page}>
@@ -63,7 +38,7 @@ function index() {
           </div>
         </div>
 
-        <div className={styles.page__contents__imageBox}>{cardList}</div>
+        <div className={styles.page__contents__imageBox}>{CARD_LIST}</div>
       </div>
       {/* 공통 푸터 UI 부분 */}
       <Footer />
---
+++ src/recoil/selectors/imageSelectors.ts
@@ -0,0 +1,27 @@
+import { selector } from "recoil";
+
+import axios from "axios";
+import { searchState } from "../atoms/searchState";
+import { pageState } from "../atoms/pageState";
+const API_URL = "https://api.unsplash.com/search/photos";
+const API_key = "BQxx4yY-cWYtU5z5C3dZB8GuBU_GbLKDjupgrCJPcG4";
+const PER_PAGE = 30;
+
+export const imageData = selector({
+  key: "imageData",
+  get: async ({ get }) => {
+    const searchValue = get(searchState);
+    const pageValue = get(pageState);
+
+    // API 호출
+    try {
+      const res = await axios.get(
+        `${API_URL}?query=${searchValue}&client_id=${API_key}&page=${pageValue}&per_page=${PER_PAGE}`
+      );
+      console.log(res);
+      return res;
+    } catch (error) {
+      console.log(error);
+    }
+  },
+});
