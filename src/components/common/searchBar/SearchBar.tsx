afc11f9d2f58c0cae3d79d1324623570e9888b38
kangsuyeong <ekdh16@naver.com>
2025-01-11 22:00:28 +0900

    feat : 17강 완료

--- src/components/common/searchBar/SearchBar.tsx
+++ src/components/common/searchBar/SearchBar.tsx
@@ -1,6 +1,36 @@
+import { useState } from "react";
 import styles from "./searchBar.module.scss";
+import { useRecoilState } from "recoil";
+import { searchState } from "@/recoil/atoms/searchState";
 
 function SearchBar() {
+  const [search, setSearch] = useRecoilState(searchState);
+  const [text, setText] = useState("");
+
+  const onChange = (event) => {
+    console.log(event.target.value);
+    setText(event.target.value);
+  };
+
+  const onSearch = () => {
+    if (text === "") {
+      // input 태그 안에 빈 값을 검색하였을 때 => default searching
+      setSearch("Korea");
+    } else {
+      setSearch(text); // 작성한 Input value 값 전달
+    }
+  };
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      if (text === "") {
+        // input 태그 안에 빈 값을 검색하였을 때 => default searching
+        setSearch("Korea");
+      } else {
+        setSearch(text); // 작성한 Input value 값 전달
+      }
+    }
+  };
+
   return (
     <div className={styles.searchBar}>
       <div className={styles.searchBar__search}>
@@ -8,8 +38,10 @@ function SearchBar() {
           type="text"
           placeholder="찾으실 이미지를 검삭하세요."
           className={styles.searchBar__search__input}
+          onChange={onChange}
+          onKeyDown={handleKeyDown}
         />
-        <img src="src/assets/icons/icon-search.svg" alt="" />
+        <img src="src/assets/icons/icon-search.svg" alt="" onClick={onSearch} />
       </div>
     </div>
   );
