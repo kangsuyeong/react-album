d0ebb879621b29dd192602a093da1b63bf5b5d3e
kangsuyeong <ekdh16@naver.com>
2025-01-12 15:23:46 +0900

    feat : 19강 완료

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,6 +1,12 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./Header.module.scss";
 
 export default function Header() {
+  const navigate = useNavigate();
+  // 북마크 페이지로 이동
+  const moveToPage = () => {
+    navigate("/bookmark");
+  };
   return (
     <header className={styles.header}>
       {/* 로고박스 */}
@@ -15,7 +21,12 @@ export default function Header() {
       {/* 프로필 박스 */}
       <div className={styles.header__profileBox}>
         <button className={styles.header__profileBox__button}>사진제출</button>
-        <button className={styles.header__profileBox__button}>북마크</button>
+        <button
+          className={styles.header__profileBox__button}
+          onClick={moveToPage}
+        >
+          북마크
+        </button>
         <span className={styles.header__profileBox__userName}>강수영</span>
       </div>
     </header>
